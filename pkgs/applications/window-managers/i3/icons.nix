836f08ece555680d15e8ff47bca0b790d84cb571
Sara Johnsson <46252070+Fendse@users.noreply.github.com>
2021-01-31 15:14:55 +0100

    Predictably enough, that didn't fix the src

--- pkgs/applications/window-managers/i3/icons.nix
+++ pkgs/applications/window-managers/i3/icons.nix
@@ -1,5 +1,5 @@
-{ fetchgit, i3, stdenv, lib, ... }:
-let iconPatch = fetchgit {
+{ pkgs, i3, stdenv, lib, ... }:
+let iconPatch = pkgs.fetchgit {
   url = "https://aur.archlinux.org/i3-wm-iconpatch.git";
   rev = "695e3ad65c0579ddbc7bf71e3f4678bf3f2273f3";
   sha256 = "0b8h4ris0igbcchj1my4p854myaz7fpg84iq2ssg5qhi0nyyqczq";
@@ -9,6 +9,11 @@ in
     pname = "i3-icons";
 
     version = "4.18.3";
+
+    src = pkgs.fetchurl {
+      url = "https://i3wm.org/downloads/i3-${version}.tar.bz2";
+      sha256 = "03dijnwv2n8ak9jq59fhq0rc80m5wjc9d54fslqaivnnz81pkbjk";
+    };
 
     patches = (lib.attrsets.attrByPath [ "patches" ] [] oldAttrs)
       ++ [
